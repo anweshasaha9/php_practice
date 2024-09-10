cdac6c9e2234d702ef85b73b7c82fa5b102798be
anweshasaha9 <anwesha@digitalpolygon.com>
2024-09-10 21:03:58 +0530

    OA-132: Task 4.

--- OA-132/task4.php
+++ OA-132/task4.php
@@ -7,7 +7,38 @@
 </head>
 <body>
   <?php
-    
+    function addStudent(&$students, $name, $grade ) {
+      if ( empty($name) || !is_numeric($grade) || $grade < 0 || $grade > 100 ) {
+        echo "Please provide correct details.";
+        return;
+      }
+      $students[$name] = $grade;
+    }
+
+    function highestGrade($students) {
+      if (empty($students)) {
+        echo "No student found.";
+      }
+      $highestGrade = -1;
+      $topper = null;
+
+      foreach ($students as $name => $grade) {
+        if ($grade > $highestGrade) {
+          $highestGrade = $grade;
+          $topper = $name;
+        }
+      }
+      return [$topper, $highestGrade];
+    }
+    // Sample Students
+    addStudent( $students, "Anwesha", 90);
+    addStudent( $students, "Ishita", 80);
+    addStudent( $students, "Kiko", 100);
+    addStudent( $students, "Poco", 100);
+    print_r($students);
+
+    list($name, $grade) = highestGrade($students);
+    echo "The highest to score is $name, $grade </br>";
   ?>
 </body>
 </html>
